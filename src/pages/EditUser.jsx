22f15c04e0c814e52a98277f558642ea30139d25
walther <waltherpires@hotmail.com>
2024-11-11 22:59:06 -0300

    modificacoes em EditUser

--- src/pages/EditUser.jsx
+++ src/pages/EditUser.jsx
@@ -28,31 +28,29 @@ export default function EditUser(){
 
   return (
     <div className="flex justify-center items-center h-[90vh]"> 
-      <div className="w-1/3 p-6 shadow-lg bg-white rounded-md"> 
+      <form className="w-1/3 p-6 shadow-lg bg-white rounded-md"> 
         <h1 className="text-3xl block text-center font-semibold"><i class="fa-solid fa-user mx-2"></i>Editar Usuário</h1> 
         <hr className="mt-3"/>      
         <div className="mt-3">
-          <label for="username" className="block text-base mb-2">Nome</label>
+          <label htmlFor="username" className="block text-base mb-2">Nome</label>
           <input type="text" id="username" className="border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600" value={user.name}/>  
         </div>
         <div className="mt-3">
-          <label for="password" className="block text-base mb-2">Nova Senha</label>
+          <label htmlFor="password" className="block text-base mb-2">Nova Senha</label>
           <input type="password" id="password" className="border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600" placeholder="Digite sua nova senha"/>  
         </div>
         <div className="mt-3">
-          <label for="email" className="block text-base mb-2">E-mail</label>
+          <label htmlFor="email" className="block text-base mb-2">E-mail</label>
           <input type="email" id="email" className="border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600" value={user.email}/>
         </div>
         <div className="mt-3">
-          <label for="tipo" className="block text-base mb-2">Tipo de Usuário</label>
+          <label htmlFor="tipo" className="block text-base mb-2">Tipo de Usuário</label>
           <input type="text" id="tipo" className="border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600" value={user.typeOfUser}/>  
         </div>
         <div className="mt-5">
           <button type="submit" className="border-2 py-1 rounded-md w-full font-semibold bg-neutral-500 text-white hover:bg-neutral-900">Salvar</button>
         </div>
-      </div>
-
-
+      </form>
     </div>
 
   )
